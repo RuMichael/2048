0279621bf371fea32625a79322cd4ac170cbf231
RuMonty <world.ru.monty@gmail.com>
2019-04-26 12:19:27 +0300

    rework swappoint

--- Assets/Scripts/Main_v2.cs
+++ Assets/Scripts/Main_v2.cs
@@ -6,45 +6,23 @@ using UnityEngine.UI;
 public class Main_v2 : MonoBehaviour
 {
 
-    enum Direction
+    enum Direction : byte
     {
-        Up,Down,Left,Right,NULL
+        Up=1,Down=2,Left=3,Right=4,NULL=0
     }
     Direction direction = Direction.NULL;
     bool newPoint = true;
     int[,] points = new int[4,4];
     public Text[] HudPoint = new Text[16];
 
-    int size = 4;
+    const int size = 4;
 
     void Start()
     {
         for(int i=0;i<4;i++)
             for(int j=0;j<4;j++)
                 points[i,j]=0;
-        //CreateNewPoint(0,0,8);
-        //CreateNewPoint(0,1,4);
-        //CreateNewPoint(0,2,2);
-        //CreateNewPoint(0,3,2);
-
-        //CreateNewPoint(1,0,2);
-        //CreateNewPoint(1,1,2);
-        //CreateNewPoint(1,2,4);
-        //CreateNewPoint(1,3,8);
-
-        //CreateNewPoint(0, 0, 8);
-        //CreateNewPoint(1, 0, 4);
-        //CreateNewPoint(2, 0, 2);
-        //CreateNewPoint(3, 0, 2);
-
-        //CreateNewPoint(0, 1, 0);
-        //CreateNewPoint(1, 1, 2);
-        //CreateNewPoint(2, 1, 2);
-        //CreateNewPoint(3, 1, 4);
-
-        //CreateNewPoint(0, 2, 4);
-        //CreateNewPoint(0, 3, 4);
-
+        direction = Direction.NULL;
         CreateNewPoint();
         newPoint = true;
         CreateNewPoint();
@@ -54,22 +32,20 @@ public class Main_v2 : MonoBehaviour
     void Update()
     {
         PressKey();
-        ShowGrid();
     }
     void CreateNewPoint()
     {        
-        if (!CheckGrid() || !newPoint)
-            return;
+        //if (!CheckGrid() || !newPoint)
+            //return;
         int rndPosition;
         int posX, posY;
-
         do
         {
             rndPosition = Random.Range(0,16);
             posX = rndPosition%4;
             posY = rndPosition/4;
         }
-        while (points[posX,posY] != 0 && CheckGrid()); 
+        while (points[posX,posY] != 0); 
         int val = Random.Range(0,11);
         points[posX,posY]  = (val<10)? 2:4;
         newPoint=false;
@@ -82,11 +58,30 @@ public class Main_v2 : MonoBehaviour
     bool CheckGrid()
     {
         foreach (var item in points)        
-            if (item == 0) return true;        
+            if (item == 0) return true;   
+        CheckGameOver();     
         return false;
     }
-    void SwapPoints(Direction direction)
+    void SwapPoints()
     {
+        /*System.Func<int, int, Point> convert;
+        switch (direction) {
+            case Direction.Up:
+                convert = ConvertUp;
+                break;
+            case Direction.Down:
+                convert = ConvertDown;
+                break;
+            case Direction.Left:
+                convert = ConvertLeft;
+                break;
+            case Direction.Right:
+                convert = ConvertRight;
+                break;
+            default:
+                return;
+        }*/
+
 
         if (direction == Direction.NULL)
             return;
@@ -98,6 +93,9 @@ public class Main_v2 : MonoBehaviour
             for (int j = 1; j < size; j++)
             {
                 int x, y;
+
+                //Point point = convert(i, j);
+
                 if (direction == Direction.Up || direction == Direction.Down)
                 {
                     x = i;
@@ -112,11 +110,12 @@ public class Main_v2 : MonoBehaviour
 
                 while (((direction == Direction.Right) ? x + 1 < size : (direction == Direction.Left) ? x - 1 >= 0 : (direction == Direction.Up) ? y + 1 < size : y - 1 >= 0) && points[x, y] != 0)
                 {
-                    if (points[((direction == Direction.Up || direction == Direction.Down) ? x : (direction == Direction.Left) ? x - 1 : x + 1), ((direction == Direction.Left || direction == Direction.Right) ? y : (direction == Direction.Up) ? y + 1 : y - 1)] == 0)
-                        points[(direction == Direction.Up || direction == Direction.Down) ? x : (direction == Direction.Left) ? x - 1 : x + 1, (direction == Direction.Left || direction == Direction.Right) ? y : (direction == Direction.Up) ? y + 1 : y - 1] = points[x, y];
-                    else if (points[x, y] == points[(direction == Direction.Up || direction == Direction.Down) ? x : (direction == Direction.Left) ? x - 1 : x + 1, (direction == Direction.Left || direction == Direction.Right) ? y : (direction == Direction.Up) ? y + 1 : y - 1] && sum)
+                    
+                    if (points[FindCorrectPosition(x,"x"), FindCorrectPosition(y, "y")] == 0)
+                        points[FindCorrectPosition(x,"x"), FindCorrectPosition(y, "y")] = points[x, y];
+                    else if (points[x, y] == points[FindCorrectPosition(x,"x"), FindCorrectPosition(y, "y")] && sum)
                     {
-                        points[(direction == Direction.Up || direction == Direction.Down) ? x : (direction == Direction.Left) ? x - 1 : x + 1, (direction == Direction.Left || direction == Direction.Right) ? y : (direction == Direction.Up) ? y + 1 : y - 1] += points[x, y];
+                        points[FindCorrectPosition(x,"x"), FindCorrectPosition(y, "y")] += points[x, y];
                         sum = false;
                     }
                     else
@@ -131,14 +130,50 @@ public class Main_v2 : MonoBehaviour
                         x = (direction == Direction.Left) ? x - 1 : x + 1;
                     newPoint = true;
                 }
-                
             }
         }
+        if (CheckGrid())        
+            if (newPoint)
+                CreateNewPoint();
         
-
-        CreateNewPoint();
-        newPoint = true;
     }
+
+/* 
+    private struct Point {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y) {
+            X = x;
+            Y = x;
+        }
+    
+    }
+
+    private Point ConvertLeft(int i, int j) {
+        return new Point(size - i - 1, j);
+    }
+
+    private Point ConvertRight(int i, int j) {
+        return new Point(i, j);
+    }
+
+    private Point ConvertUp(int i, int j) {
+        return new Point(size - i - 1, j);
+    }
+
+    private Point ConvertDown(int i, int j) {
+        return new Point(j, i);
+    }*/
+
+
+
+    int FindCorrectPosition(int value, string s)
+    {
+        return (s == "x") ? (direction == Direction.Up || direction == Direction.Down) ? value : (direction == Direction.Left) ? value - 1 : value + 1 :
+        (direction == Direction.Left || direction == Direction.Right) ? value : (direction == Direction.Down) ? value - 1 : value + 1;
+    }
+
     void PressKey()
     {
         direction=Direction.NULL;
@@ -151,26 +186,39 @@ public class Main_v2 : MonoBehaviour
                     else if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
                         direction = Direction.Right;
         if (direction!= Direction.NULL)
-        {
-            //newPoint = true;
-            SwapPoints(direction);
-        }
-    }
-    bool CheckPosition(int posX, int posY)
-    {
-        return points[posX,posY] == 0;            
-    }
-    bool CheckPosition(out int value)
-    {
-        value=3;
-        return true;
-    }
+            SwapPoints();   
+        ShowGrid();  
+    }    
 
     void ShowGrid()
     {
         for (int x=0;x<4;x++)
             for (int y=0;y<4;y++)            
                 HudPoint[y*4 + x].text = (points[x,y] != 0)? points[x,y].ToString():"";
+    }
+
+    void CheckGameOver()
+    {
+        int[,] tmpPoints = points;
+        for (byte i=1;i<5;i++)
+        {
+            direction = (Direction) i;
+            SwapPoints();
+            if (!CheckEquality(tmpPoints))
+                points = tmpPoints;
+            else 
+                Start();
+        }
+
+    }
+
+    bool CheckEquality(int [,] tmp)
+    {
+        for (int i = 0 ; i<size; i++)
+            for (int j = 0; j<size; j++)
+                if (tmp[i,j] != points[i,j])
+                    return false;
+        return true;
     }
 
 }
